04490a951794cbfac312e397d01b767b062fc3d0
Michelle <michelle.pine33@gmail.com>
2019-11-01 16:06:09 -0400

    navbar finished, high-level routing finished

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styles from './Navbar.scss';
+import './Navbar.scss';
+import { Link, NavLink } from 'react-router-dom';
 
-const Navbar = props => {
-  return (
-    <div>
-    </div>
-  );
+class Navbar extends React.Component {
+  isActive(match, location) {
+    if (!match) {
+      return false;
+    }
+
+    return match.url === "/";
+  }
+
+  render() {
+    return (
+      <nav className="nav-bar">
+        <div className="nav-logo">
+          <Link className="nav-logo-link" to="/">CoLab</Link>
+        </div>
+        <div className="nav-link-group">
+          <ul className="nav-links list-unstyled">
+            <li className="nav-link-wrapper">
+              <NavLink activeClassName="active" isActive={this.isActive} className="nav-link" to="/">All Projects</NavLink>
+            </li>
+            <li className="nav-link-wrapper">
+              <NavLink activeClassName="active" className="nav-link" to="/create-project">Create Project</NavLink>
+            </li>
+            <li className="nav-link-wrapper">
+              <NavLink activeClassName="active" className="nav-link" to="/my-profile">My Profile</NavLink>
+            </li>
+            <li className="nav-link-wrapper">
+              <Link className="nav-link" to="/login">Logout</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
 };
 
 Navbar.defaultProps = {
--- src/index.js
+++ src/index.js
@@ -3,28 +3,37 @@ import ReactDOM from 'react-dom';
 import { Route, BrowserRouter as Router } from 'react-router-dom'
 
 import './index.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.js';
 
+//pages
 import AllProjects from './pages/AllProjects/AllProjects';
 import CreateProject from './pages/CreateProject/CreateProject';
 import MyProfile from './pages/MyProfile/MyProfile';
 import Project from './pages/Project/Project';
 import NotFound from './pages/NotFound/NotFound';
+import Login from './pages/Login/Login';
 
+//components
+import Navbar from './components/Navbar/Navbar';
 
 import * as serviceWorker from './serviceWorker';
 
 const routing = (
+  <div className="page-wrapper">
   <Router>
+    <Navbar />
     <div>
       <Route exact path="/" component={AllProjects} />
-      <Route path="/projects" component={AllProjects} />
       <Route path="/my-profile" component={MyProfile} />
       <Route path="/create-project" component={CreateProject} />
       <Route path="/edit-project" component={CreateProject} />
       <Route path="/projects/:id" component={Project} />
+      <Route path="/login" component={Login} />
       <Route component={NotFound} />
     </div>
   </Router>
+  </div>
 )
 
 ReactDOM.render(routing, document.getElementById('root'))
