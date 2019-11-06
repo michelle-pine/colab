a745ed2dbaee8d6557a6d9de465bb99bfce81a21
Michelle <michelle.pine33@gmail.com>
2019-11-06 00:41:07 -0500

    Updates

---
+++ precache-manifest.c60d1021d491b753221ffc307a2da633.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23fc4d893fad3445e03a255549905352",
+    "url": "/colab/index.html"
+  },
+  {
+    "revision": "95e2b82616d02c0172ff",
+    "url": "/colab/static/css/2.b708a4dd.chunk.css"
+  },
+  {
+    "revision": "72ff651d78820ef7cbe9",
+    "url": "/colab/static/css/main.9531a1a4.chunk.css"
+  },
+  {
+    "revision": "95e2b82616d02c0172ff",
+    "url": "/colab/static/js/2.c65dc021.chunk.js"
+  },
+  {
+    "revision": "72ff651d78820ef7cbe9",
+    "url": "/colab/static/js/main.0f6970e7.chunk.js"
+  },
+  {
+    "revision": "1749d5975afadb7409d3",
+    "url": "/colab/static/js/runtime-main.aa6f4a28.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/colab/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/colab/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/colab/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/colab/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/colab/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
